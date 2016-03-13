2191ddb5b703212d3ca464b9ec83cddc8650d65c
trackstar200 <tim.stoddard2@gmail.com>
2016-03-12 20:53:24 -0800

    added features
    * move fast
    * minimap
    * background sound

--- src/EngineFrame.java
+++ src/EngineFrame.java
@@ -2,11 +2,12 @@
 /**
  * Frame to display the engine
  * 
- * @author Tim Stoddard
+ * @author Tim Stoddard, Sam Lindbloom-Airey
  * @version program007
  */
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Color;
 import java.awt.Font;
 
@@ -21,242 +22,275 @@ import java.text.DecimalFormat;
 
 public class EngineFrame extends JFrame {
 
-	// double buffering
-	private Image dbImage;
-	private Graphics dbg;
-	private Map map;
-	private Player player;
-	private Scores scores;
-	private boolean cheatMode;
-	private int currLevel;
-	private long startTime;
+   private Image dbImage; // for double buffering
+   private Graphics dbg;
+   private Map map;
+   private Player player;
+   private Scores scores;
+   private boolean cheatMode;
+   private int currLevel;
+   private long startTime;
 
-	/**
-	 * Creates a new EngineFrame to display and render the game.
-	 */
-	public EngineFrame() {
-		super("CPE Quest");
+   /**
+    * Creates a new EngineFrame to display and render the game.
+    */
+   public EngineFrame() {
+      super("CPE Quest");
 
-		add(new JPanel() {
+      add(new JPanel() {
 
-			public void paint(Graphics window) {
-				// don't edit this
-				dbImage = createImage(getWidth(), getHeight());
-				dbg = dbImage.getGraphics();
-				paintComponent(dbg);
-				window.drawImage(dbImage, 0, 0, this);
-			}
+         public void paint(Graphics window) {
 
-			public void paintComponent(Graphics g) {
-				// actually painting goes here
+            /* don't edit this */
+            dbImage = createImage(getWidth(), getHeight());
+            dbg = dbImage.getGraphics();
+            paintComponent(dbg);
+            window.drawImage(dbImage, 0, 0, this);
+            /* end don't edit this */
+         }
 
-				/* temporary graphics */
-				int scale = 25;
-				java.awt.Graphics2D g2 = (java.awt.Graphics2D) g;
-				for (int i = 0; i < map.getHeight(); i++) {
-					for (int j = 0; j < map.getWidth(); j++) {
-						int type = map.getTile(i, j).getType();
-						if (type == Tile.SPACE) {
-							g.setColor(Color.white);
-						} else if (type == Tile.WALL) {
-							g.setColor(Color.black);
-						} else if (type == Tile.FINISH) {
-							g.setColor(Color.red);
-						} else if (type == Tile.SHOW_PATH) {
-							if (!cheatMode) {
-								g2.setColor(Color.white);
-							} else {
-								g2.setColor(Color.yellow);
-							}
-						}
-						g2.fillRect(i * scale, j * scale, scale, scale);
-					}
-				}
-				g2.setColor(Color.green);
-				double angle = Math.toRadians(360 - player.getDirection() + 90);
-				g2.rotate(angle, player.getPos().x * scale, player.getPos().y * scale);
-				g2.setColor(Color.red);
-				g2.fillPolygon(
-						new int[] { (int) (player.getPos().x * scale) - 4, (int) (player.getPos().x * scale),
-								(int) (player.getPos().x * scale) + 4 },
-						new int[] { (int) (player.getPos().y * scale) + 5, (int) (player.getPos().y * scale) - 5,
-								(int) (player.getPos().y * scale) + 5 },
-						3);
-				g2.rotate(-angle, player.getPos().x * scale, player.getPos().y * scale);
-				g2.setColor(Color.cyan);
-				g2.fillRect(505, 8, 105, 100);
-				g2.setColor(Color.black);
-				g2.setFont(new Font("Helvetica", Font.PLAIN, 30));
-				g2.drawString("Level " + currLevel, 510, 40);
-				long ms = System.currentTimeMillis() - startTime;
-				g2.drawString(new DecimalFormat("##.00").format((System.currentTimeMillis() - startTime) / 1000.0), 510,
-						80);
-				/* end of temporary graphics */
-			}
-		});
+         public void paintComponent(Graphics g) {
+            
+            Graphics2D g2 = (Graphics2D) g;
 
-		addKeyListener(new KeyListener() {
+            drawMiniMap(g2, 500, 0);
+         }
+      });
 
-			@Override
-			public void keyTyped(KeyEvent e) {
-				// nothing here!
-			}
+      addKeyListener(new KeyListener() {
 
-			@Override
-			public void keyPressed(KeyEvent e) {
-				int key = e.getKeyCode();
-				switch (key) {
-				case KeyEvent.VK_UP:
-				case KeyEvent.VK_W:
-					player.setForward(true);
-					break;
-				case KeyEvent.VK_DOWN:
-				case KeyEvent.VK_S:
-					player.setBackward(true);
-					break;
-				case KeyEvent.VK_RIGHT:
-				case KeyEvent.VK_D:
-					player.setRight(true);
-					break;
-				case KeyEvent.VK_LEFT:
-				case KeyEvent.VK_A:
-					player.setLeft(true);
-					break;
-				case KeyEvent.VK_C:
-					cheatMode = !cheatMode;
-					break;
-				/*case KeyEvent.VK_1:
-					updateMap(1);
-					break;
-				case KeyEvent.VK_2:
-					updateMap(2);
-					break;
-				case KeyEvent.VK_3:
-					updateMap(3);
-					break;
-				case KeyEvent.VK_4:
-					updateMap(4);
-					break;*/
-				case KeyEvent.VK_Q:
-					player.setRotateLeft(true);
-					break;
-				case KeyEvent.VK_E:
-					player.setRotateRight(true);
-					break;
-				}
-			}
+         @Override
+         public void keyTyped(KeyEvent e) {
+            // nothing here!
+         }
 
-			@Override
-			public void keyReleased(KeyEvent e) {
-				int key = e.getKeyCode();
-				switch (key) {
-				case KeyEvent.VK_UP:
-				case KeyEvent.VK_W:
-					player.setForward(false);
-					break;
-				case KeyEvent.VK_DOWN:
-				case KeyEvent.VK_S:
-					player.setBackward(false);
-					break;
-				case KeyEvent.VK_RIGHT:
-				case KeyEvent.VK_D:
-					player.setRight(false);
-					break;
-				case KeyEvent.VK_LEFT:
-				case KeyEvent.VK_A:
-					player.setLeft(false);
-					break;
-				case KeyEvent.VK_Q:
-					player.setRotateLeft(false);
-					break;
-				case KeyEvent.VK_E:
-					player.setRotateRight(false);
-					break;
-				}
-			}
-		});
+         @Override
+         public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+               case KeyEvent.VK_UP:
+               case KeyEvent.VK_W:
+                  player.setForward(true);
+                  break;
+               case KeyEvent.VK_DOWN:
+               case KeyEvent.VK_S:
+                  player.setBackward(true);
+                  break;
+               case KeyEvent.VK_RIGHT:
+               case KeyEvent.VK_D:
+                  player.setRight(true);
+                  break;
+               case KeyEvent.VK_LEFT:
+               case KeyEvent.VK_A:
+                  player.setLeft(true);
+                  break;
+               case KeyEvent.VK_C:
+                  cheatMode = !cheatMode;
+                  break;
 
-		scores = new Scores();
-		init();
+               /* remove this later */
+               case KeyEvent.VK_1:
+                  updateMap(1);
+                  break;
+               case KeyEvent.VK_2:
+                  updateMap(2);
+                  break;
+               case KeyEvent.VK_3:
+                  updateMap(3);
+                  break;
+               case KeyEvent.VK_4:
+                  updateMap(4);
+                  break;
+               /* end remove this later */
 
-		setSize(700, 700);
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+               case KeyEvent.VK_Q:
+                  player.setRotateLeft(true);
+                  break;
+               case KeyEvent.VK_E:
+                  player.setRotateRight(true);
+                  break;
+               case KeyEvent.VK_SPACE:
+                  player.moveFast();
+                  break;
+            }
+         }
 
-	public void init() {
-		currLevel = 1;
-		updateMap(currLevel);
-		cheatMode = false;
-		startTime = System.currentTimeMillis();
-	}
+         @Override
+         public void keyReleased(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+               case KeyEvent.VK_UP:
+               case KeyEvent.VK_W:
+                  player.setForward(false);
+                  break;
+               case KeyEvent.VK_DOWN:
+               case KeyEvent.VK_S:
+                  player.setBackward(false);
+                  break;
+               case KeyEvent.VK_RIGHT:
+               case KeyEvent.VK_D:
+                  player.setRight(false);
+                  break;
+               case KeyEvent.VK_LEFT:
+               case KeyEvent.VK_A:
+                  player.setLeft(false);
+                  break;
+               case KeyEvent.VK_Q:
+                  player.setRotateLeft(false);
+                  break;
+               case KeyEvent.VK_E:
+                  player.setRotateRight(false);
+                  break;
+            }
+         }
+      });
 
-	public void nextLevel() {
-		long totalTime = System.currentTimeMillis() - startTime;
-		currLevel++;
-		if (currLevel > 4) { // user wins!
-			scores.addNewHighScore(scores.calcScore(totalTime), stripNonAlpha((String) JOptionPane.showInputDialog(this,
-					"Congrats! You win!\nPlease enter your name.", null, JOptionPane.PLAIN_MESSAGE, null, null, null)));
+      scores = new Scores();
+      init();
 
-			// pause for dramatic effect
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				// do nothing #yolo
-			}
+      setSize(700, 700);
+      setVisible(true);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   }
 
-			Object[] options = { "Play again!", "Quit" };
-			if (JOptionPane.showOptionDialog(this,
-					"HIGH SCORES\n" + scores.getHighScores(3) + "---------\nWould you like to play again or quit?",
-					null, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]) == 0) {
-				init();
-			} else {
-				System.exit(1);
-			}
-		}
+   public void init() {
+      currLevel = 1;
+      updateMap(currLevel);
+      cheatMode = false;
+      startTime = System.currentTimeMillis();
+   }
 
-		// pause for dramatic effect
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			// do nothing #yolo
-		}
+   public void nextLevel() {
+      long totalTime = System.currentTimeMillis() - startTime;
+      currLevel++;
+      if (currLevel > 4) { // user wins!
+         scores.addNewHighScore(scores.calcScore(totalTime),
+               stripNonAlpha((String) JOptionPane.showInputDialog(this,
+                     "Congrats! You win!\nPlease enter your name.", null,
+                     JOptionPane.PLAIN_MESSAGE, null, null, null)));
 
-		// load up the new level
-		map = new Map(currLevel);
-		player = new Player(map);
-	}
+         // pause for dramatic effect
+         try {
+            Thread.sleep(500);
+         } catch (InterruptedException e) {
+            // do nothing #yolo
+         }
 
-	private String stripNonAlpha(String text) {
-		char[] chars = text.toCharArray();
-		for (int i = 0; i < chars.length; i++) {
-			if (!Character.isAlphabetic(chars[i]) && !Character.isDigit(chars[i]) && chars[i] != 32) {
-				chars[i] = '.';
-			}
-		}
-		return new String(chars).replace(".", "");
-	}
+         Object[] options = { "Play again!", "Quit" };
+         if (JOptionPane.showOptionDialog(this,
+               "HIGH SCORES\n" + scores.getHighScores(3)
+                     + "---------\nWould you like to play again or quit?",
+               null, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
+               null, options, options[0]) == 0) {
+            init();
+         } else {
+            System.exit(1);
+         }
+      }
 
-	/**
-	 * Returns the current map.
-	 * 
-	 * @return The current map
-	 */
-	public Map getMap() {
-		return map;
-	}
+      // pause for dramatic effect
+      try {
+         Thread.sleep(500);
+      } catch (InterruptedException e) {
+         // do nothing #yolo
+      }
 
-	/**
-	 * Returns the player.
-	 * 
-	 * @return the player
-	 */
-	public Player getPlayer() {
-		return player;
-	}
+      // load up the new level
+      map = new Map(currLevel);
+      player = new Player(map);
+   }
 
-	private void updateMap(int level) {
-		map = new Map(level);
-		player = new Player(map);
-	}
+   private String stripNonAlpha(String text) {
+      char[] chars = text.toCharArray();
+      for (int i = 0; i < chars.length; i++) {
+         if (!Character.isAlphabetic(chars[i]) && !Character.isDigit(chars[i])
+               && chars[i] != 32) {
+            chars[i] = '.';
+         }
+      }
+      return new String(chars).replace(".", "");
+   }
+
+   /**
+    * Returns the current map.
+    * 
+    * @return The current map
+    */
+   public Map getMap() {
+      return map;
+   }
+
+   /**
+    * Returns the player.
+    * 
+    * @return the player
+    */
+   public Player getPlayer() {
+      return player;
+   }
+
+   private void updateMap(int level) {
+      map = new Map(level);
+      player = new Player(map);
+   }
+   
+   private void drawMiniMap(Graphics2D g2, int dx, int dy) {
+      int scale = 10;
+      for (int i = 0; i < map.getWidth(); i++) {
+         System.out.println("i" + i);
+         for (int j = 0; j < map.getWidth(); j++) {
+            System.out.print("j" + j + " ");
+            int type = map.getTile(i, j).getType();
+            if (type == Tile.SPACE) {
+               g2.setColor(Color.white);
+            } else if (type == Tile.WALL) {
+               g2.setColor(Color.black);
+            } else if (type == Tile.FINISH) {
+               g2.setColor(Color.red);
+            } else if (type == Tile.SHOW_PATH) {
+               if (!cheatMode) {
+                  g2.setColor(Color.white);
+               } else {
+                  g2.setColor(Color.yellow);
+               }
+            }
+            g2.fillRect(i * scale + dx, j * scale + dy, scale, scale);
+         }
+      }
+
+      // draw the player
+      double angle = Math.PI * 5 / 2 - player.getDirection();
+      g2.rotate(angle, player.getPos().x * scale + dx,
+            player.getPos().y * scale + dy);
+      g2.setColor(Color.red);
+      g2.fillPolygon( // replace this with bird's eye raycasting view
+            new int[] { (int) (player.getPos().x * scale) - 4 + dx,
+                  (int) (player.getPos().x * scale) + dx,
+                  (int) (player.getPos().x * scale) + 4 + dx },
+            new int[] { (int) (player.getPos().y * scale) + 5 + dy,
+                  (int) (player.getPos().y * scale) - 5 + dy,
+                  (int) (player.getPos().y * scale) + 5 + dy },
+            3);
+      g2.rotate(-angle, player.getPos().x * scale + dx,
+            player.getPos().y * scale + dy);
+      
+      // draw stats
+      g2.setColor(Color.cyan);
+      g2.fillRect(505 + dx, 8 + dy, 105, 100);
+      g2.setColor(Color.black);
+      g2.setFont(new Font("Helvetica", Font.PLAIN, 30));
+      g2.drawString("Level " + currLevel, 510 + dx, 40 + dy);
+      g2.drawString(
+            new DecimalFormat("##.00").format(
+                  (System.currentTimeMillis() - startTime) / 1000.0),
+            510 + dx, 80 + dy);
+      
+      // draw move fast status
+      if (player.canMoveFast()) {
+         g2.setColor(Color.green);
+      } else {
+         g2.setColor(Color.red);
+      }
+      g2.fillRect(510 + dx, 150 + dy, 30, 30);
+   }
 }
--- src/Scores.java
+++ src/Scores.java
@@ -1,3 +1,4 @@
+
 /**
  * Reads, writes, and stores all the scores
  * 
@@ -16,152 +17,156 @@ import java.util.ArrayList;
 
 public class Scores {
 
-	private ArrayList<Score> scores;
+   private ArrayList<Score> scores;
 
-	/**
-	 * Constructs an object in which to hold all the scores.
-	 */
-	public Scores() {
-		scores = new ArrayList<Score>();
-		read();
-	}
+   /**
+    * Constructs an object in which to hold all the scores.
+    */
+   public Scores() {
+      scores = new ArrayList<Score>();
+      read();
+   }
 
-	/**
-	 * Gets the specified number of high scores, or all high scores if there are
-	 * less than the specified number. Returns in String format.
-	 * 
-	 * @return the specified number of top high scores
-	 */
-	public String getHighScores(int howMany) {
-		if (howMany < 0) {
-			throw new ArrayIndexOutOfBoundsException("negative index: " + howMany);
-		}
-		String text = "";
-		int num = Math.min(howMany, scores.size());
-		for (int i = 0; i < num; i++) {
-			text += scores.get(i).score + " (" + scores.get(i).name + ")\n";
-		}
-		return text;
-	}
+   /**
+    * Gets the specified number of high scores, or all high scores if there are
+    * less than the specified number. Returns in String format.
+    * 
+    * @return the specified number of top high scores
+    */
+   public String getHighScores(int howMany) {
+      if (howMany < 0) {
+         throw new ArrayIndexOutOfBoundsException(
+               "negative index: " + howMany);
+      }
+      String text = "";
+      int num = Math.min(howMany, scores.size());
+      for (int i = 0; i < num; i++) {
+         text += scores.get(i).score + " (" + scores.get(i).name + ")\n";
+      }
+      return text;
+   }
 
-	/**
-	 * Calculates a score based on the time parameter.
-	 * 
-	 * @param ms
-	 *            - The time to complete the level, in milliseconds
-	 * @return The score of the level
-	 */
-	public int calcScore(long ms) {
-		return Math.max((int) (42069420 / (ms / 1000.0 + (1.0 / Integer.MAX_VALUE))), 0);
-	}
+   /**
+    * Calculates a score based on the time parameter.
+    * 
+    * @param ms
+    *           - The time to complete the level, in milliseconds
+    * @return The score of the level
+    */
+   public int calcScore(long ms) {
+      return Math.max(
+            (int) (42069420 / (ms / 1000.0 + (1.0 / Integer.MAX_VALUE))), 0);
+   }
 
-	/**
-	 * Adds a new high score to the list. The score must be between
-	 * 
-	 * @param newScore
-	 *            - score that was earned.
-	 * @param name
-	 *            - name associated with the score
-	 */
-	public void addNewHighScore(int newScore, String name) {
-		scores.add(new Score(newScore, name));
-		
-		// selection sort
-		for (int i = 0; i < scores.size(); i++) {
-			int maxPos = i;
-			for (int j = i + 1; j < scores.size(); j++) {
-				if (scores.get(j).compareTo(scores.get(maxPos)) < 0) {
-					maxPos = j;
-				}
-			}
-			Score temp = scores.get(i);
-			scores.set(i, scores.get(maxPos));
-			scores.set(maxPos, temp);
-		}
+   /**
+    * Adds a new high score to the list. The score must be between
+    * 
+    * @param newScore
+    *           - score that was earned.
+    * @param name
+    *           - name associated with the score
+    */
+   public void addNewHighScore(int newScore, String name) {
+      scores.add(new Score(newScore, name));
 
-		// write to file
-		BufferedWriter out;
-		try {
-			out = new BufferedWriter(new FileWriter(new File("").getAbsolutePath() + "/src/high_scores.txt"));
-			for (int i = 0; i < scores.size(); i++) {
-				out.write(scores.get(i).score + "," + scores.get(i).name + "\n");
-			}
-			out.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found.");
-		} catch (IOException e) {
-			System.out.println("IO Error occurred.");
-		}
-	}
+      // selection sort
+      for (int i = 0; i < scores.size(); i++) {
+         int maxPos = i;
+         for (int j = i + 1; j < scores.size(); j++) {
+            if (scores.get(j).compareTo(scores.get(maxPos)) < 0) {
+               maxPos = j;
+            }
+         }
+         Score temp = scores.get(i);
+         scores.set(i, scores.get(maxPos));
+         scores.set(maxPos, temp);
+      }
 
-	/**
-	 * Reads the score data from the text file.
-	 */
-	private void read() {
-		BufferedReader in;
-		try {
-			in = new BufferedReader(new FileReader(new File("").getAbsolutePath() + "/src/high_scores.txt"));
-			ArrayList<String> data = new ArrayList<String>();
-			String text;
-			while ((text = in.readLine()) != null) {
-				data.add(text);
-			}
-			in.close();
-			for (int i = 0; i < data.size(); i++) {
-				String[] tempData = data.get(i).split(",");
-				scores.add(new Score(Integer.parseInt(tempData[0]), tempData[1]));
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found.");
-		} catch (IOException e) {
-			System.out.println("IO Error occurred.");
-		} catch (java.lang.NumberFormatException e) {
-			System.out.println("Score was too large.");
-		}
-	}
+      // write to file
+      BufferedWriter out;
+      try {
+         out = new BufferedWriter(new FileWriter(
+               new File("").getAbsolutePath() + "/src/high_scores.txt"));
+         for (int i = 0; i < scores.size(); i++) {
+            out.write(scores.get(i).score + "," + scores.get(i).name + "\n");
+         }
+         out.close();
+      } catch (FileNotFoundException e) {
+         System.out.println("File not found.");
+      } catch (IOException e) {
+         System.out.println("IO Error occurred.");
+      }
+   }
 
-	/**
-	 * Represents a score object. Has a score and a name associated with that
-	 * score.
-	 * 
-	 * @author timstoddard
-	 *
-	 */
-	private class Score implements Comparable<Score> {
+   /**
+    * Reads the score data from the text file.
+    */
+   private void read() {
+      BufferedReader in;
+      try {
+         in = new BufferedReader(new FileReader(
+               new File("").getAbsolutePath() + "/src/high_scores.txt"));
+         ArrayList<String> data = new ArrayList<String>();
+         String text;
+         while ((text = in.readLine()) != null) {
+            data.add(text);
+         }
+         in.close();
+         for (int i = 0; i < data.size(); i++) {
+            String[] tempData = data.get(i).split(",");
+            scores.add(new Score(Integer.parseInt(tempData[0]), tempData[1]));
+         }
+      } catch (FileNotFoundException e) {
+         System.out.println("File not found.");
+      } catch (IOException e) {
+         System.out.println("IO Error occurred.");
+      } catch (java.lang.NumberFormatException e) {
+         System.out.println("Score was too large.");
+      }
+   }
 
-		int score;
-		String name;
+   /**
+    * Represents a score object. Has a score and a name associated with that
+    * score.
+    * 
+    * @author timstoddard
+    *
+    */
+   private class Score implements Comparable<Score> {
 
-		/**
-		 * Creates a new Score.
-		 * 
-		 * @param score
-		 * @param name
-		 */
-		public Score(int score, String name) {
-			this.score = score;
-			this.name = name;
-		}
+      int score;
+      String name;
 
-		/**
-		 * Compares this score to another. If the scores are different, the
-		 * larger one comes first; if they are the same, the one whose name
-		 * lexicographically precedes the other comes first.
-		 */
-		@Override
-		public int compareTo(Score other) {
-			if (other == null) {
-				return Integer.MAX_VALUE;
-			}
-			if (other.getClass() != this.getClass()) {
-				return Integer.MAX_VALUE;
-			}
-			if (this.score > other.score) {
-				return -1;
-			} else if (this.score < other.score) {
-				return 1;
-			}
-			return this.name.compareTo(other.name);
-		}
-	}
+      /**
+       * Creates a new Score.
+       * 
+       * @param score
+       * @param name
+       */
+      public Score(int score, String name) {
+         this.score = score;
+         this.name = name;
+      }
+
+      /**
+       * Compares this score to another. If the scores are different, the larger
+       * one comes first; if they are the same, the one whose name
+       * lexicographically precedes the other comes first.
+       */
+      @Override
+      public int compareTo(Score other) {
+         if (other == null) {
+            return Integer.MAX_VALUE;
+         }
+         if (other.getClass() != this.getClass()) {
+            return Integer.MAX_VALUE;
+         }
+         if (this.score > other.score) {
+            return -1;
+         } else if (this.score < other.score) {
+            return 1;
+         }
+         return this.name.compareTo(other.name);
+      }
+   }
 }
