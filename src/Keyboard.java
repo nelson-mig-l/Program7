f6b5e4e6c310884ff657f838731b802035d2e6cd
Sam Lindbloom-Airey <sam@pcp143725pcs.wireless.calpoly.edu>
2016-03-15 00:19:52 -0700

    removed obsolete movement code

--- src/Keyboard.java
+++ src/Keyboard.java
@@ -27,33 +27,27 @@ public class Keyboard implements KeyListener {
         switch (key) {
             case KeyEvent.VK_UP:
             case KeyEvent.VK_W:
-                player.setForward(true);
                 player.speed = 1;
                 break;
             case KeyEvent.VK_DOWN:
             case KeyEvent.VK_S:
-                player.setBackward(true);
                 player.speed = -1;
                 break;
             case KeyEvent.VK_RIGHT:
             case KeyEvent.VK_D:
-                player.setRight(true);
                 player.sideSpeed = 1;
                 break;
             case KeyEvent.VK_LEFT:
             case KeyEvent.VK_A:
-                player.setLeft(true);
                 player.sideSpeed = -1;
                 break;
             case KeyEvent.VK_C:
                 player.toggleCheatMode();
                 break;
             case KeyEvent.VK_Q:
-                player.setRotateLeft(true);
                 player.rotating = -1;
                 break;
             case KeyEvent.VK_E:
-                player.setRotateRight(true);
                 player.rotating = 1;
                 break;
             case KeyEvent.VK_SPACE:
@@ -68,30 +62,24 @@ public class Keyboard implements KeyListener {
         switch (key) {
             case KeyEvent.VK_UP:
             case KeyEvent.VK_W:
-                player.setForward(false);
                 player.speed = 0;
                 break;
             case KeyEvent.VK_DOWN:
             case KeyEvent.VK_S:
-                player.setBackward(false);
                 player.speed = 0;
                 break;
             case KeyEvent.VK_RIGHT:
             case KeyEvent.VK_D:
-                player.setRight(false);
                 player.sideSpeed = 0;
                 break;
             case KeyEvent.VK_LEFT:
             case KeyEvent.VK_A:
-                player.setLeft(false);
                 player.sideSpeed = 0;
                 break;
             case KeyEvent.VK_Q:
-                player.setRotateLeft(false);
                 player.rotating = 0;
                 break;
             case KeyEvent.VK_E:
-                player.setRotateRight(false);
                 player.rotating = 0;
                 break;
         }
